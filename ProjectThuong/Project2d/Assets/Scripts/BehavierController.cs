a48960b019ab648a42d7e52de34ae551ee1d7873
TrinhVanThuong <anhtomthuong99@gmail.com>
2019-10-01 20:25:18 +0700

    ABC
    dichuyen

---
+++ ProjectThuong/Project2d/Assets/Scripts/BehavierController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class BehavierController : MonoBehaviour
+
+{
+    /* [SerializeField]
+    protected float durationmove = 0.5f; 
+    private LeanTweenType typemove = LeanTweenType.linear;
+    
+    int id;
+    protected void MoveTo(Vector3 EndPoint,Action onComplete = null)
+    {
+        LeanTween.moveLocal(this.gameObject,EndPoint,durationmove).setEase(typemove).setOnComplete(onComplete);
+
+    }
+    protected void MoveUpdate(Vector3 EndPoint)
+    {
+        LeanTween.cancel (id);
+        id = LeanTween.moveLocal (gameObject,EndPoint,durationmove).setEase(typemove).id;
+    }*/
+ 
+}
---
+++ ProjectThuong/Project2d/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : PlaneController
+{
+    public float speed = 50f,maxspeed = 3, jumpPow = 200f;
+    public bool grounded = true, faceright = true;
+    public Rigidbody2D r2;
+
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        r2 = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetBool("Grounded",grounded);
+        anim.SetFloat("speed",Mathf.Abs(r2.velocity.x));
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if (grounded)
+            {
+                grounded = false; 
+                r2.AddForce(Vector2.up * jumpPow);
+            }
+            
+        }
+
+    }
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        r2.AddForce((Vector2.right) *speed * h);
+//gioi han toc do
+
+        if(r2.velocity.x > maxspeed)
+        r2.velocity = new Vector2(maxspeed,r2.position.y);
+        if(r2.velocity.x < -maxspeed)
+        r2.velocity = new Vector2(maxspeed,r2.position.y);
+        
+        if(h>0 && !faceright)
+        {
+            Flip();
+        }
+        if(h< 0 && !faceright)
+        {
+            Flip();
+        }
+    }
+    public void Flip()
+    {
+        faceright = !faceright;
+        Vector3 Scale;
+        Scale = transform.localScale;
+        Scale.x *= -1;
+        transform.localScale = Scale;
+
+
+    }
+    void OnCollisionEnter2D(Collision2D collisionInfo)
+    {
+        if(collisionInfo.gameObject.CompareTag("Ground"))
+        {
+            grounded = true;
+        }
+    }
+}
