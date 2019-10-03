f363d5bd8ec5ba388568b4aae4498f0a68e37af9
tth851993 <36069038+tth851993@users.noreply.github.com>
2019-10-04 02:38:21 +0700

    add bullet

---
+++ ROTMG-fake/Assets/Scripts/BulletController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+    Animator animator;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.magnitude > 10.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void Launch(Vector2 direction, float force)
+    {
+        animator.SetFloat("MoveX", direction.x);
+        animator.SetFloat("MoveY", direction.y);
+        rigidbody2d.AddForce(direction * force);
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        //we also add a debug log to know what the projectile touch
+        Debug.Log("Projectile Collision with " + other.gameObject);
+        Destroy(gameObject);
+    }
+}
--- ROTMG-fake/Assets/Scripts/PlayerController.cs
+++ ROTMG-fake/Assets/Scripts/PlayerController.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : PersonController
 {
     Rigidbody2D rigidbody2d;
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1,0);
     public int maxHealth = 5;
     public float speed = 3.0f;
     int currentHealth;
+    public GameObject bulletPrefab;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -20,17 +24,42 @@ public class PlayerController : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
        float vertical = Input.GetAxis("Vertical");
-       Vector2 position = transform.position;
-       position.x = position.x + speed * horizontal * Time.deltaTime;
-       position.y = position.y + speed * vertical * Time.deltaTime;
+       Vector2 move = new Vector2(horizontal, vertical);
+
+       if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("MoveX", lookDirection.x);
+        animator.SetFloat("MoveY", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+       Vector2 position = rigidbody2d.position;
+    
+       position = position + move * speed * Time.deltaTime;
 
        rigidbody2d.MovePosition(position);
+
+       if(Input.GetKeyDown(KeyCode.I))
+        {
+            Launch();
+        }
     }
 
-    void ChangeHealth(int amount)
+    // void ChangeHealth(int amount)
+    // {
+    //     currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+    //     Debug.Log(currentHealth + "/" + maxHealth);
+    // }
+    void Launch()
     {
-        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        Debug.Log(currentHealth + "/" + maxHealth);
+        GameObject projectileObject = Instantiate(bulletPrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        BulletController projectile = projectileObject.GetComponent<BulletController>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Shoot");
     }
-    
 }
