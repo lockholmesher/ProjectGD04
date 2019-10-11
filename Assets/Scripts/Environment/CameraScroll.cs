b0a1d53b58053f7f7f631f7c84bda192a1d7ed3b
Khanhdrag9 <tkhanh783@gmail.com>
2019-10-11 16:37:26 +0700

    Added change speed when player is near bottom

--- Assets/Scripts/Environment/CameraScroll.cs
+++ Assets/Scripts/Environment/CameraScroll.cs
@@ -3,19 +3,35 @@ using UnityEngine;
 public class CameraScroll : BaseObject
 {
     public GameObject target;
+    public float speedChange;
+    public Range rangeRateDrop;
+    
+    float targetRate = 1;
+
+    void Start()
+    {
+        targetRate = speedRateDrop;
+    }
+
     protected override void Update()
     {
         base.Update();
+        
+        if(targetRate < speedRateDrop)
+            speedRateDrop = Mathf.Clamp(speedRateDrop - speedChange * Time.deltaTime, rangeRateDrop.min, rangeRateDrop.max);
+        else if(targetRate > speedRateDrop)
+            speedRateDrop = Mathf.Clamp(speedRateDrop + speedChange * Time.deltaTime, rangeRateDrop.min, rangeRateDrop.max);
+
         if(target)
         {   
             if(target.transform.position.y < transform.position.y)
             {
                 float more = Mathf.Abs(transform.position.y - target.transform.position.y) / (Screen.height * 0.5f);
-                speedRateDrop = Mathf.Clamp(speedRateDrop + more, 0.75f, 1.5f);
+                targetRate = rangeRateDrop.max;
             }
             else
             {
-                speedRateDrop = 1;
+                targetRate = 1;
             }
         }
     }
